0d7738c4a09f09928794c22730f5cba6219b33bd
nareshkorvi <78070530+nareshkorvi@users.noreply.github.com>
2023-08-06 22:07:09 +0530

    middle of section 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
